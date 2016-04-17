1249c42b0cf5407ef9c95760643fd135b1b3245d
Brian Bier <briansbier@gmail.com>
2016-04-17 10:38:53 -0400

    Create user table and model

---
+++ app/models/user.rb
@@ -0,0 +1,22 @@
+class User < ActiveRecord::Base
+  has_secure_password
+  has_many :posts
+  validates :first_name, :last_name, :username, presence: true
+  validates :email, presence: true, uniqueness: true, length: { minimum: 4 }
+  validates :password, presence: true, length: { in: 6..8} 
+  validates :bio, length: { in: 6..10 }
+  validate :email_validator
+
+
+
+
+
+
+private
+
+  def email_validator
+    unless email && email.match(/[^!@#$%^&*()~]+[^!@#$%^&*()~0.9]+.com/)
+      errors.add(:email, 'is not valid')
+    end
+  end
+end
