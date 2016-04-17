c064828e8d5108382ae2207ef45dcd8406d275f5
Brian Bier <briansbier@gmail.com>
2016-04-17 10:53:55 -0400

    Create model and migration for images

---
+++ db/migrate/20160417144734_createimages.rb
@@ -0,0 +1,11 @@
+class Createimages < ActiveRecord::Migration
+  def change
+    create_table :posts do |t|
+      t.text :content,
+      t.references :post
+      t.references :user
+
+      t.timestamps null: false
+    end
+  end
+end
